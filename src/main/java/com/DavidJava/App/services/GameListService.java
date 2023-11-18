6d5a0b121b9ce3499bd450ed2819bbb0efd81dd5
David Flávio <davidflaviolm1982@gmail.com>
2023-11-18 18:53:34 -0300

    Customization Game position implemented

--- src/main/java/com/DavidJava/App/services/GameListService.java
+++ src/main/java/com/DavidJava/App/services/GameListService.java
@@ -4,19 +4,38 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.DavidJava.App.dto.GameListDTO;
 import com.DavidJava.App.entities.GameList;
+import com.DavidJava.App.projections.GameMinProjection;
 import com.DavidJava.App.repositories.GameListRepository;
+import com.DavidJava.App.repositories.GameRepository;
 
 @Service
 public class GameListService {
   @Autowired
   private GameListRepository gameListRepository;
+  @Autowired
+  private GameRepository gameRepository;
 
-  public List<GameListDTO> findAll(){
+  @Transactional(readOnly = true)
+  public List<GameListDTO> findAll() {
     List<GameList> result = gameListRepository.findAll();
     return result.stream().map(x -> new GameListDTO(x)).toList();
   }
-  
+  @Transactional
+  public void move(Long listId, int sourceIndex, int destinationIndex) {
+    List<GameMinProjection> list = gameRepository.searchByList(listId);
+    GameMinProjection obj = list.remove(sourceIndex);
+    list.add(destinationIndex, obj);
+
+    int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+    int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+    for(int i = min; i <= max; i++){
+      gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+    }
+  }
+
 }
